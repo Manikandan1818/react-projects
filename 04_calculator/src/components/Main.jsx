4c755b6f2f3d2f9562bb6becf9d185efcc154b29
Manikandan1818 <simbumanii18@gmail.com>
2023-11-26 13:26:58 +0530

    Calculator task completed!

--- 04_calculator/src/components/Main.jsx
+++ 04_calculator/src/components/Main.jsx
@@ -4,28 +4,45 @@ import "./style.css";
 const Main = () => {
   const [inputValue, setInputValue] = useState("");
 
+  const display = (value) => {
+    setInputValue(inputValue + value);
+  };
+
+  const calculate = () => {
+    let answers = eval(inputValue);
+    setInputValue(answers);
+  };
+
+  const clear = () => {
+    setInputValue("");
+  };
+
   return (
     <>
       <form name="calc" className="calculator">
         <input type="text" className="value" value={inputValue} />
-        <span className="num clear">C</span>
-        <span>/</span>
-        <span>*</span>
-        <span>7</span>
-        <span>8</span>
-        <span>9</span>
-        <span>-</span>
-        <span>4</span>
-        <span>5</span>
-        <span>6</span>
-        <span className="num plus">+</span>
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
-        <span>0</span>
-        <span>00</span>
-        <span>.</span>
-        <span className="num equal">=</span>
+        <span onClick={() => display("7")}>7</span>
+        <span onClick={() => display("8")}>8</span>
+        <span onClick={() => display("9")}>9</span>
+        <span onClick={() => display("/")}>/</span>
+        <span onClick={() => display("4")}>4</span>
+        <span onClick={() => display("5")}>5</span>
+        <span onClick={() => display("6")}>6</span>
+        <span onClick={() => display("*")}>*</span>
+        <span onClick={() => display("1")}>1</span>
+        <span onClick={() => display("2")}>2</span>
+        <span onClick={() => display("3")}>3</span>
+        <span onClick={() => display("-")}>-</span>
+        <span onClick={() => display("0")}>0</span>
+        <span onClick={() => display("00")}>00</span>
+        <span onClick={() => display(".")}>.</span>
+        <span onClick={() => display("+")}>+</span>
+        <span className="num" onClick={() => clear()}>
+          C
+        </span>
+        <span onClick={() => calculate()} className="num">
+          =
+        </span>
       </form>
     </>
   );
