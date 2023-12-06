8c239809f1fddb85dbce6df33d9c54dbc7cc3074
Manikandan1818 <simbumanii18@gmail.com>
2023-12-06 19:38:34 +0530

    Category component working

--- 10_ecommerce/src/Sidebar/Category/Category.jsx
+++ 10_ecommerce/src/Sidebar/Category/Category.jsx
@@ -1,7 +1,33 @@
 import "./Category.css";
 
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <div>
+      <h2 className="sidebar-title">Category</h2>
+      <div>
+        <label className="sidebar-label">
+          <input type="radio" name="test" />
+          <span className="checkmark">All</span>
+        </label>
+        <label className="sidebar-label">
+          <input type="radio" name="test" />
+          <span className="checkmark">Sneakers</span>
+        </label>
+        <label className="sidebar-label">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Flats
+        </label>
+        <label className="sidebar-label">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sandals
+        </label>
+        <label className="sidebar-label">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Heels
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
--- 10_ecommerce/src/Sidebar/Sidebar.js
+++
@@ -1,7 +0,0 @@
-import "./sidebarr.css";
-
-const Sidebar = () => {
-  return <div>Sidebar</div>;
-};
-
-export default Sidebar;
